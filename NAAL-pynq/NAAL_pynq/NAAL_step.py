491b31175bdc16307dcfaf32e2cdb4cb8fcab73e
Ki-Cheol <jja147@naver.com>
2020-05-04 15:28:20 +0900

    Update NAAL_step.py

--- NAAL-pynq/NAAL_pynq/NAAL_step.py
+++ NAAL-pynq/NAAL_pynq/NAAL_step.py
@@ -1,123 +1,97 @@
+from __future__ import print_function
+import os
+import argparse
+import atexit
+import os
+import argparse
 import socket
-import sys
-import threading
-import numpy as np
-from enum import Enum
-class board_command(Enum):
-    INIT=0
-    START=1
-    PROCEEDING=2
-    PAUSE=3
-    STOP=4
-class socket_type(Enum):
-    SEND=0
-    RECV=1
-    NONE=3
-class naal_socket(object):
-    def __init__(self, IP_addr,dimensions,type=socket_type.RECV ,command=board_command.INIT):
-        self.IP_addr =IP_addr
-        self.type=type
-        self.dimensions=dimensions
+from pes_driver import PESDriver
+from utils.paths import params_path
+from NAAL_step import naal_socket
+from NAAL_step import NAAL_UDPnetwork
+import time
 
-        self.message = np.empty(self.dimensions+2)
-        self.message[0] = np.nan
-        
-        self._socket = None
-        self.open()
+def cleanup(fpga_driver, socket_step):
+    # Terminate the fpga driver
+    fpga_driver.terminate()
 
-    def __call__(self):
-        if self.type.value == socket_type.RECV.value :
-            self.open()
-            self._socket.bind(self.IP_addr)
-        else :
-            self.open()
+    # Close and cleanup the UDP socket
+    socket_step.close()
+print("NAAL_FPGA board start")
 
-    @property
-    def command(self):
-        return self.message[0]
-    def set_command(self,command):
-        self.message[0]=command.value
-    @property
-    def dt(self):
-        return self.message[1]
-    @property
-    def closed(self):
-        return self._socket is None
-    def open(self):
-        try :
-            self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            if sys.platform.startswith('bsd') or sys.platform.startswith('darwin'):
-                self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-            else:
-                self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+parser = argparse.ArgumentParser(
+    description='Generic for running the pes network on the fpga board.')
 
-        except ValueError : 
-            print("socket errorIP :"+IP_addr+" socket exit")
-            return
-    def recv(self):
-        self._socket.recv_into(self.message.data)
+# Socket communication arguments
+parser.add_argument(
+    '--host_ip', type=str, default='127.0.0.1',
+    help='IP Address of host PC. .')
+parser.add_argument(
+    '--remote_ip', type=str, default='127.0.0.1',
+    help='IP Address of FPGA board. Used for socket communication')
+parser.add_argument(
+    '--udp_port', type=int, default=500000,
+    help='Port number to use for the socket communication.')
+parser.add_argument(
+    '--in_dimensions', type=int, default=0,
+    help='.')
+parser.add_argument(
+    '--out_dimensions', type=int, default=0,
+    help='.')    
+parser.add_argument(
+    '--socket_args', type=str, default='{}',
+    help='Additional arguments used when creating the udp socket. Should be '+
+    'formatted as a dictionary string.')
+parser.add_argument(
+    '--tcp_port', type=int, default=50000,
+    help='Port number to use for the socket communication.')
 
-    def send(self,dt, vector_data,command=board_command.PROCEEDING):
+parser.add_argument(
+    '--arg_data_file', type=str, default=os.path.join(params_path,'args.npz'),
+    help='path to npz file arguments data file ')
 
-        self.message[0] =  command.value
-        self.message[1] = dt
-        self.message[2:] = vector_data
-        self._socket.sendto(self.message.tobytes(), self.IP_addr)
 
-class NAAL_UDPnetwork(object):
-    def __init__(self, host_IP,remote_IP,in_demesion,out_demension):
-        self.host_IP=host_IP
-        self.remote_IP=remote_IP
-        self.in_demesion=in_demesion
-        self.out_demension=out_demension
-        #command [0] step[1]
-        self.dt=0.0
-        self.recv=naal_socket(self.remote_IP,self.in_demesion)
-        self.send =naal_socket(self.host_IP,self.out_demension,socket_type.SEND)
-        self.recv()
-        self.send()
-        initarr=[]
-        for i in range(0,self.out_demension):
-           initarr.append(0.0)
-        tuple(initarr)
-        self.send.send(0,initarr,board_command.START)
-        self.send.set_command(board_command.PROCEEDING)
-        self.recv.recv()
-        print("board recv:")
-        print(self.recv.message)
+args = parser.parse_args()
+fpga_driver = PESDriver(args.arg_data_file)
+print("udp socket create")
+print("remote_addr = "+args.host_ip)
+print("listen_addr = "+args.remote_ip)
+remote_addr=(args.host_ip, args.udp_port)
+listen_addr=(args.remote_ip, args.udp_port)
 
-        # print(self.recv.message)
-        # print("while")
-        #self.send()
-        # print("test")
-        # self.recv.recv()
-        # while True :
-        #     self.send.send(0,initarr,board_command.START)
-        #     print("asdasd")
-        # self.send.send(0,initarr,board_command.START)
-        # self.send.set_command(board_command.PROCEEDING)
-        # self.dt=0.0
-
-    def step_call_recv(self,dt):
-        self.recv.recv()
-        if dt !=self.recv.message[1] : 
-            while True :
-                self.recv.recv()
-                if dt ==self.recv.message[1]:
-                    break;
-        self.recv.set_command(board_command.PROCEEDING)
-        print("board recv :")
-        print(self.recv.message)
-        return self.recv.message
-
-    def step_call_send(self,dt,vector_data):
-        self.send.send(dt,vector_data)
+atexit.register(cleanup, fpga_driver)
+curr_t = 0
+dt = fpga_driver.dt
 
 
 
 
-        
+print("dt value = "+str(dt))
+a=NAAL_UDPnetwork(remote_addr,listen_addr,args.in_dimensions,args.out_dimensions)
+fpga_driver.update_input_error_values(a.recv.vector)
+output_value=fpga_driver.step()
+while not fpga_driver.stopped:
+    recv_data=a.step_call_recv(curr_t)
+    if recv_data[0] == 3:
+        continue
+    elif recv_data[0] == 4 :
+        fpga_driver.stopped= True
+
+    print("recv_data[1:] =")
+    print(recv_data[1:])
+    fpga_driver.update_input_error_values(recv_data[2:])
+    output_value=fpga_driver.step()
+    print("output_value")
+    print(output_value)
+    a.step_call_send(curr_t,output_value)
+    curr_t += dt
+
+    
 
 
-        
-        
+# while not fpga_driver.stopped:
+#     socket_recv_value = socket_step(curr_t, fpga_driver.output)
+#     fpga_driver.update_input_error_values(socket_recv_value)
+#     output_value = fpga_driver.step()
+#     # Update the current timestamp
+#     curr_t += dt
